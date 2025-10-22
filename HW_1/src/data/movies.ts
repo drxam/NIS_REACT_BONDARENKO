ac05623ff8b9f1c117b4628e90ecf379baf2b2f5
Dmitriy <DreadDima12345@yandex.ru>
2025-10-22 15:14:30 +0300

    initial project structure

---
+++ HW_1/src/data/movies.ts
@@ -0,0 +1,39 @@
+import { Movie } from '../types/movie';
+
+export const initialMovies: Movie[] = [
+    {
+        id: 1,
+        title: 'Побег из Шоушенка',
+        year: 1994,
+        posterUrl: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/0b76b2a2-d1c7-4f04-a284-80ff7bb709a4/600x900',
+        isFavorite: false,
+    },
+    {
+        id: 2,
+        title: 'Крестный отец',
+        year: 1972,
+        posterUrl: 'https://avatars.mds.yandex.net/get-kinopoisk-image/10703959/ebf237d7-64a3-4b75-98f1-d228d54658e5/600x900',
+        isFavorite: false,
+    },
+    {
+        id: 3,
+        title: 'Темный рыцарь',
+        year: 2008,
+        posterUrl: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/0fa5bf50-d5ad-446f-a599-b26d070c8b99/600x900',
+        isFavorite: false,
+    },
+    {
+        id: 4,
+        title: 'Криминальное чтиво',
+        year: 1994,
+        posterUrl: 'https://avatars.mds.yandex.net/get-kinopoisk-image/4716873/0a07a903-9025-4aff-bf7c-46bbb175888c/600x900',
+        isFavorite: false,
+    },
+    {
+        id: 5,
+        title: 'Форрест Гамп',
+        year: 1994,
+        posterUrl: 'https://avatars.mds.yandex.net/get-kinopoisk-image/1599028/3560b757-9b95-45ec-af8c-623972370f9d/600x900',
+        isFavorite: false,
+    },
+];
